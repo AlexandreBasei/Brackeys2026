caa11ae32326c1e87fcc070bd474ca6221e532a2
Antoine Vuillet <antoine.vuillet@etu.univ-grenoble-alpes.fr>
2026-08-25 14:18:15 +0200

    addition of other ghost comportement

--- Assets/Scripts/BystanderScript.cs
+++ Assets/Scripts/BystanderScript.cs
@@ -11,8 +11,12 @@ public class BystanderScript : MonoBehaviour
     private GameObject targetNode;
     public GameObject Player;
     private bool isPossessed = false;
+    private bool isFaking = false;
     private bool isStill = false;
     [SerializeField] private Animator animator;
+    public GameObject neutralMask;
+    public GameObject grumpyMask;
+    public GameObject happyMask;
 
     void Start()
     {
@@ -30,7 +34,7 @@ public class BystanderScript : MonoBehaviour
     void Update()
     {
         animator.SetFloat("Speed", agent.velocity.magnitude);
-        if (!isPossessed)
+        if (!isPossessed && !isFaking)
         {
             if (!isStill)
             {
@@ -65,12 +69,62 @@ public class BystanderScript : MonoBehaviour
         isPossessed = true;
         targetNode = Player;
         agent.SetDestination(targetNode.transform.position);
-        agent.speed = 20f;
-        agent.acceleration = 16f;
-        yield return new WaitForSeconds(Random.Range(3f, 6f));  
+        agent.speed = 13f;
+        agent.acceleration = 10f;
+        neutralMask.SetActive(false);
+        grumpyMask.SetActive(true);
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+        grumpyMask.SetActive(false);
+        neutralMask.SetActive(true);
         isPossessed = false;
         agent.speed = 5f;
         agent.acceleration = 8f;
+        ChoseTarget();
+    }
+
+    public IEnumerator Fakeout()
+    {
+        isFaking = true;
+        targetNode = Player;
+        agent.SetDestination(targetNode.transform.position);
+        agent.speed = 13f;
+        agent.acceleration = 10f;
+        neutralMask.SetActive(false);
+        happyMask.SetActive(true);
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+        happyMask.SetActive(false);
+        neutralMask.SetActive(true);
+        isFaking = false;
+        agent.speed = 5f;
+        agent.acceleration = 8f;
+        ChoseTarget();
+    }
+
+    public IEnumerator Feral()
+    {
+        isPossessed = true;
+        targetNode = Player;
+        agent.SetDestination(targetNode.transform.position);
+        agent.speed = 17f;
+        agent.acceleration = 16f;
+        neutralMask.SetActive(false);
+        grumpyMask.SetActive(true);
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+        grumpyMask.SetActive(false);
+        neutralMask.SetActive(true);
+        isPossessed = false;
+        agent.speed = 5f;
+        agent.acceleration = 8f;
+        ChoseTarget();
+    }
+
+    public IEnumerator Smart()
+    {
+        isPossessed = true;
+        targetNode = Player;
+        agent.SetDestination(targetNode.transform.position);
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+        isPossessed = false;
         ChoseTarget();
     }
 
