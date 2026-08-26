68b35a12330ef6b88987989d947fffdcc9bef2a5
Bryan Albani <bryanalbani1999@gmail.com>
2026-08-27 00:23:51 +1100

    Changed how work the ambient sound

--- Assets/Scripts/SoundAmbient/SoundAmbient.cs
+++ Assets/Scripts/SoundAmbient/SoundAmbient.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class SoundAmbient : MonoBehaviour
 {
     [Header("-------Audio Source-------")]
-    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioSource[] audioSource;
 
     [Header("-------Audio Clips-------")]
-    [SerializeField] private AudioClip audioClips;
+    [SerializeField] private AudioClip TVSound;
+    [SerializeField] private AudioClip PianoSound;
+    [SerializeField] private AudioClip GramophoneSound;
 
     [Header("-------Random Delay-------")]
     [SerializeField] private float minDelay = 45f;
@@ -18,6 +20,30 @@ public class SoundAmbient : MonoBehaviour
         StartCoroutine(PlayRandomSound());
     }
 
+    public void PlayAmbiant()
+    {
+        int randomIndex = Random.Range(0, (audioSource.Length)+1);
+        switch (randomIndex)
+        {
+            case 0:
+                audioSource[0].PlayOneShot(TVSound);
+                break;
+            case 1:
+                audioSource[1].PlayOneShot(TVSound);
+                break;
+            case 2:
+                audioSource[2].PlayOneShot(PianoSound);
+                break;
+            case 3:
+                audioSource[3].PlayOneShot(GramophoneSound);
+                break;
+            case 4:
+                audioSource[4].PlayOneShot(GramophoneSound);
+                break;
+        }
+    }
+
+
     private IEnumerator PlayRandomSound()
     {
        while(true)
@@ -25,7 +51,7 @@ public class SoundAmbient : MonoBehaviour
             float delay = Random.Range(minDelay, maxDelay);
             yield return new WaitForSeconds(delay);
 
-            audioSource.PlayOneShot(audioClips);
+            PlayAmbiant();
         } 
     }
 }
